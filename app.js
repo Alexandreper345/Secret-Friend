484ee8ce62e4f6a9569ad6a8b4456462a28564df
alexandreper345 <asilvaperoba@gmail.com>
2025-03-16 14:40:10 -0300

    fiz um jogo do jogo secreto, teve ta com algumas coisas que podem ser melhorados

---
+++ app.js
@@ -0,0 +1,35 @@
+//O principal bjetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+
+var amigos = [];
+let list = document.querySelector(".name-list");
+
+
+function adicionarAmigo()
+{
+    let input = document.getElementById("amigo");
+    
+    if (input && input.value.trim !== '')
+    {
+        amigos.push(input.value.trim());
+        input.value = '';
+        list.innerHTML = amigos.map(amigos => `<li>${amigos}</li>`).join('');
+    }
+}
+
+function sortearAmigo()
+{
+    list.innerHTML = '';
+    if (amigos.length === 0) {
+        alert("Adicione amigos antes de sortear!");
+        return; // Se não houver amigos, não sorteia
+    }
+    let result = document.querySelector(".result-list");
+    let index = Math.floor(Math.random()) * amigos.length;
+    let = drawn = amigos[index];
+
+    if (drawn !== undefined && drawn !== '')
+        result.innerHTML = `o amigo sorteado é ${drawn}`;
+    else
+        result.innerHTML = `[ERROR]`;
+
+}
